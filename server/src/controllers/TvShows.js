1dd9b0eda059843009966d044392e7baae1af1e4
Haseeb Iqbal <haseebiqbal33@gmail.com>
2022-03-26 13:41:45 +0500

    Complete backend work

--- server/src/controllers/TvShows.js
+++ server/src/controllers/TvShows.js
@@ -1,19 +1,64 @@
 const fs = require("fs");
-const path = require('path')
-const csv = require('fast-csv')
+const path = require("path");
+const csv = require("fast-csv");
 
+function capitalizeName(name) {
+  const words = name.split(" ");
+
+  return words
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    .join(" ");
+}
+
+function age(age) {
+  return age.split("+")[0];
+}
 
 const getShows = async (req, res, next) => {
-  const dataPath = path.resolve(__dirname, '../../assets/tv_shows.csv')
-  
+  const params = req.params.search;
+  const dataPath = path.resolve(__dirname, "../../assets/tv_shows.csv");
+  let shows = [];
   csv
- .parseStream( fs.createReadStream(dataPath), {headers : true , coding:"utf8"})
- .on("data", function(data){
-     console.log('I am one line of data', data);
- })
- .on("end", function(){
-     console.log("done");
- });
+    .parseStream(fs.createReadStream(dataPath), {
+      headers: true,
+      coding: "utf8",
+    })
+    .on("data", function (data) {
+      shows.push(data);
+    })
+    .on("end", function () {
+      const show = shows.find(
+        (show) => show.Title === capitalizeName(req.params.search)
+      );
+      const showAge = shows.filter(
+        (show) => age(show.Age) === req.params.search
+      );
+      if (show) {
+        res.status(200).json({
+          search: params,
+          data: {
+            Name: show.Title,
+            IMDb: show.IMDb,
+            Source:
+              show.Netflix === "1"
+                ? "Netflix"
+                : show.Hulu === "1"
+                ? "Hulu"
+                : show["Prime Video"] === "1"
+                ? "Prime Vide"
+                : "Disney+",
+            Age: show.Age,
+          },
+        });
+      } else if (showAge.length) {
+        res.status(200).json({
+          search: params,
+          data: showAge,
+        });
+      }
+    });
 };
 
 module.exports = { getShows };
